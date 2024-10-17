93153191aed252bff9846d2af8b6a676c30e32fc
sajan moon <sajanmoon5@gmail.com>
2024-10-17 19:52:44 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => {
-  res.send("Hello world");
+app.use("admin", (req, res, next) => {
+  const token = "xyz";
+  const isAuthorized = token === "xyz";
+  if (!isAuthorized) {
+    res.status(401).send("unauthorized code");
+  } else {
+    next();
+  }
 });
-
-app.use("/test", (req, res) => {
-  res.send("server is running");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello is running");
-});
+app.get(
+  "/admin/profile",
+  (req, res) => {
+    res.send("Route Handler");
+  },
+  (req, res) => {
+    res.send("Route Handler 2");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is istening succesfully on 3000 port");
 });
-
